74ce2624c9e8e3e426d7937bd2431a26969322b9
Prabesh <prabesh5511@gmail.com>
2025-07-15 19:45:17 +0545

    update app

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />}></Route>
-      <Route path="/adminprabesh1" element={<Admin />}></Route>
+      <Route path="/admin" element={<Admin />}></Route>
       <Route path="/adminAddProduct" element={<AdminAddProduct />}></Route>
             <Route path="/adminAddedProducts" element={<AdminAddedProducts />}></Route>
 
